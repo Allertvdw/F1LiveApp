62d2629c835d3a181270866db8fe5c214395b5ec
Allertvdw <allertvdw@hotmail.com>
2024-03-20 12:53:34 +0100

    Reverted back to keys in models

--- F1LiveApp.Business/Models/Session.cs
+++ F1LiveApp.Business/Models/Session.cs
@@ -14,7 +14,7 @@ namespace F1LiveApp.Business.Models
         public DateTime Date_End { get; set; }
         public DateTime Date_Start { get; set; }
         public TimeSpan Gmt_Offset { get; set; }
-        public Meeting Meeting { get; set; }
+        public int Meeting_Key { get; set; }
         public string Session_Name { get; set; }
         public string Session_Type { get; set; }
     }
