353f1871dd512d352cbdd25c34e71355d1d225cf
Allertvdw <allertvdw@hotmail.com>
2024-03-20 11:43:35 +0100

    Adjusted models

--- F1LiveApp.Business/Models/Driver.cs
+++ F1LiveApp.Business/Models/Driver.cs
@@ -17,9 +17,9 @@ namespace F1LiveApp.Business.Models
         public string Full_Name { get; set; }
         public string Headshot_Url { get; set; }
         public string Last_Name { get; set; }
-        public int Meeting_Key { get; set; }
+        public Meeting Meeting { get; set; }
         public string Name_Acronym { get; set; }
-        public int Session_Key { get; set; }
+        public Session Session { get; set; }
         public string Team_Colour { get; set; }
         public string Team_Name { get; set; }
     }
--- F1LiveApp.Business/Models/Session.cs
+++ F1LiveApp.Business/Models/Session.cs
@@ -11,18 +11,11 @@ namespace F1LiveApp.Business.Models
 	{
         [Key]
 		public int Session_Key { get; set; }
-		public int Circuit_Key { get; set; }
-        public string Circuit_Short_Name { get; set; }
-        public string Country_Code { get; set; }
-        public int Country_Key { get; set; }
-        public string Country_Name { get; set; }
         public DateTime Date_End { get; set; }
         public DateTime Date_Start { get; set; }
         public TimeSpan Gmt_Offset { get; set; }
-        public string Location { get; set; }
-        public int Meeting_Key { get; set; }
+        public Meeting Meeting { get; set; }
         public string Session_Name { get; set; }
         public string Session_Type { get; set; }
-        public int Year { get; set; }
     }
 }
